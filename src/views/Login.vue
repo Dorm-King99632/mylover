df33f361036b5744e67ae23ffe9d2d5053e0327f
Dorm-King99632 <43040234+Dorm-King99632@users.noreply.github.com>
2021-01-08 22:29:14 +0800

    新建前端项目

---
+++ src/views/Login.vue
@@ -0,0 +1,142 @@
+<template>
+  <!--登录表单的容器-->
+  <div class="login_container">
+    <!--登录区域-->
+    <div class="login_box">
+      <!--头像-->
+      <div class="avatar_box">
+        <img src="../assets/img/timg.gif">
+      </div>
+      <!--表单-->
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input  v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="verifyCode">
+          <div class="verifyCode_box">
+            <el-input v-model="loginForm.verifyCode" prefix-icon="el-icon-mobile" class="verifyCode"></el-input>
+            <img src="../assets/img/msFXK1.gif" class="verifyCode_img">
+          </div>
+        </el-form-item>
+
+        <el-form-item class="login_btn">
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        loginForm: {
+          username: '',
+          password: '',
+          verifyCode: ''
+        },
+        loginRules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 5, max: 20, message: '长度在 5 到 20 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 5, max: 20, message: '长度在 5 到 20 个字符', trigger: 'blur' }
+          ],
+          verifyCode: [
+            { required: true, message: '请输入验证码', trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            console.log('submit!');
+            this.$router.push('/main')
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: aquamarine;
+}
+
+.login_box {
+  width: 450px;
+  height: 380px;
+  background-color: #FFFFFF;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box{
+    width: 130px;
+    height: 130px;
+    border: 1px solid #EEEEEE;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    margin: -65px auto;
+    background-color: #FFFFFF;
+
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #EEEEEE;
+    }
+  }
+
+  .login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+    .login_btn{
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .verifyCode_box{
+      display: flex;
+      .verifyCode{
+        width: 70%;
+        justify-content: left;
+      }
+
+      .verifyCode_img{
+        width: 30%;
+        height: 45px;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-container class="main_container">
+    <!--头部布局-->
+    <el-header>
+      <div class="left_box">
+        <img src="../assets/img/timg.gif">
+        <span>鹦鹉和鲨鱼</span>
+      </div>
+      <div class="right_box">
+        <el-dropdown>
+          <img src="../assets/img/timg.gif">
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-house">系统首页</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-ship">交流讨论</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-switch-button">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style lang="less" scoped>
+
+.main_container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  font-size: 20px;
+
+  .left_box { //需在 style 中加入 lang="less" 否则报错
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 60px;
+      height: 60px;
+      margin: 0 0 10px 15px;
+    }
+
+    span {
+      margin-left: 15px;
+    }
+
+  }
+  .right_box {
+    .el-dropdown>img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      background-color: #FFFFFF;
+      margin: 0 15px 0 0;
+      background-size: contain;
+    }
+  }
+
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+/*下拉菜单样式*/
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+
+</style>
